f2d69b02b432c09ba5098642fcffaf90994958ae
Alejandra <siemi121001@gmail.com>
2024-10-21 14:33:49 +0200

    prueba test

--- example/src/main/java/org/factoriaf5/example/Multiplicar.java
+++ example/src/main/java/org/factoriaf5/example/Multiplicar.java
@@ -1,16 +1,17 @@
 package org.factoriaf5.example;
-import java.util.Scanner;
 
 public class Multiplicar {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("De que numero quieres saber la tabla de multiplicar?");
-        int principal = scanner.nextInt();
 
-        for (int i=0; i<=10; i++)
-        {int resultado= principal * i;
-        System.out.println( principal +"*"+ i +" = "+ resultado);}
-
+    public String [] resultado (int num){
+        
+        String [] tabla = new String[10];
+        
+        for (int multiplicador=1; multiplicador <= 10; multiplicador++){
+            tabla[multiplicador-1]= num + " x " + multiplicador +" = "+(num*multiplicador);
+        }
+        return tabla;
     }
+    
+
 }
+
--- example/src/test/java/org/factoriaf5/example/MultiplicarTest.java
+++ example/src/test/java/org/factoriaf5/example/MultiplicarTest.java
@@ -1,3 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class MultiplicarTest {
     
+    @Test
+    public void testResultado() {
+
+
+        Multiplicar multiplicar = new Multiplicar();
+
+        String[] valorEsperado = {
+            "2 x 1 = 2",
+            "2 x 2 = 4",
+            "2 x 3 = 6",
+            "2 x 4 = 8",
+            "2 x 5 = 10",
+            "2 x 6 = 12",
+            "2 x 7 = 14",
+            "2 x 8 = 16",
+            "2 x 9 = 18",
+            "2 x 10 = 20"
+        };
+        
+        String[] resultadoObtenido = multiplicar.resultado(2);
+        
+    }
 }
